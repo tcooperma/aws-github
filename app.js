b7573f71bf6ca56bd35e8a3e5e51dae6dac93649
Todd Cooper <todd_cooper@yahoo.com>
2024-11-13 16:35:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello New England Pats!');
+    res.send('Hello New England!');
 });
 
 app.listen(port, () => {
